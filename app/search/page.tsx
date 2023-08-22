80bc981553c283182117f107b344d7f3f4de2427
Richard Emijere <emijere.richard@gmail.com>
2023-08-22 09:30:02 +0000

    Added search functionality

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,8 @@
-import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
+
+import getSongsByTitle from "@/actions/getSongsByTitle";
 
 interface SearchProps {
   searchParams: {
@@ -28,6 +30,7 @@ const Search = async ({ searchParams }: SearchProps) => {
           <SearchInput />
         </div>
       </Header>
+      <SearchContent songs={songs} />
     </div>
   );
 };
--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,37 @@
-'use client'
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import qs from "query-string";
+
+import useDebounce from "@/hooks/useDebounce";
+import Input from "./Input";
 
 const SearchInput = () => {
-  return (
-    <div>SearchInput</div>
-  )
-}
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
 
-export default SearchInput
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
+
+    const url = qs.stringifyUrl({
+      url: "/search",
+      query: query,
+    });
+
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <Input
+      placeholder='What song do you want to listen to?'
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+};
+
+export default SearchInput;
